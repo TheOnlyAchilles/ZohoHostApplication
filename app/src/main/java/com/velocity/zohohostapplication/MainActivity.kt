4d8021d4bba4cee5110005fe80467c54d821247b
V3JohanM <124780665+V3JohanM@users.noreply.github.com>
2024-02-01 13:29:34 +0200

    Merge branch 'master' of https://github.com/TheOnlyAchilles/ZohoHostApplication

--- app/src/main/java/com/velocity/zohohostapplication/MainActivity.kt
+++ app/src/main/java/com/velocity/zohohostapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.velocity.zohohostapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myButton: Button = findViewById(R.id.myButton)
+
+        myButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "Button clicked!", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
