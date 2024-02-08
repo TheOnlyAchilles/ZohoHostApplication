949f460036259f1951dcfbf7634794d6ddfc0f3c
V3JohanM <124780665+V3JohanM@users.noreply.github.com>
2024-02-08 13:35:46 +0200

    first working version of zoho chat

--- app/src/main/java/com/velocity/zohohostapplication/MainActivity.kt
+++ app/src/main/java/com/velocity/zohohostapplication/MainActivity.kt
@@ -1,28 +1,16 @@
 package com.velocity.zohohostapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.velocity.velocity_zoho.velocity_zoho_chat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myButton: Button = findViewById(R.id.myButton)
-        val velocityZohoChat = velocity_zoho_chat(this.application)
 
-        val oncreate = velocityZohoChat.onCreate()
-        Log.d("Hi Mom: ", oncreate.toString())
-
-        myButton.setOnClickListener {
-            Toast.makeText(
-                applicationContext,
-                velocityZohoChat.helloWorld,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
+        val appKey = "{{appKey}}";
+        val accessKey = "{{accessKey}}";
+        velocity_zoho_chat().initZoho(application, appKey, accessKey);
     }
 }
--- velocity_zoho/src/main/java/com/velocity/velocity_zoho/velocity_zoho_chat.java
+++ velocity_zoho/src/main/java/com/velocity/velocity_zoho/velocity_zoho_chat.java
@@ -2,57 +2,31 @@ package com.velocity.velocity_zoho;
 
 import android.app.Application;
 import android.util.Log;
-
 import com.zoho.commons.InitConfig;
 import com.zoho.livechat.android.listeners.InitListener;
 import com.zoho.salesiqembed.ZohoSalesIQ;
-import com.zoho.commons.Fonts;
 
 public class velocity_zoho_chat extends Application {
-    public String message = "Hello, world! MotherF*****";
-    private final Application application;
 
-    public velocity_zoho_chat(Application application) {
-        this.application = application;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
+    public void initZoho(Application application, String appKey, String accessKey){
         try {
-            // Initialize Zoho SalesIQ on the UI thread
             InitConfig initConfig = new InitConfig();
-            initConfig.setFont(Fonts.REGULAR, ""); // Specify your font here
-
-            String accessKey = "LbO1ugc6oWJuhb2lMBuvgCbX8ePG4Vah1oUY2SbehdSV7ijMF6zKKqyBa90tFrc8eLCb6%2F6tylPKHmQ9qwrgNYjBx4SjLDClQ2kmhRKuTsxF5taRliRJYyGz2avGobCKBASPR7MbSJT3JJSvtk9%2B6RPCANC9LnQa";
-            String appKey = "0YTMmRaFQkdKEEb6NxhloyN7WcoYThP9naVmXju0PF%2FC1sCwmNlvVw%3D%3D_eu";
             ZohoSalesIQ.init(application, appKey, accessKey, initConfig, new InitListener() {
                 @Override
                 public void onInitSuccess() {
-                    // Show the Zoho SalesIQ launcher
-                    Log.d("Hi Mom", "Yo");
-
+                    Log.d("LOGS", "SUCCESS");
                     ZohoSalesIQ.Launcher.show(ZohoSalesIQ.Launcher.VisibilityMode.ALWAYS);
-
-
                 }
 
                 @Override
                 public void onInitError(int errorCode, String errorMessage) {
-                    // Handle initialization error (your code)
-                    Log.d("Hi Mom", "No No");
-
+                    Log.d("LOGS", "ERROR: " + errorMessage);
                 }
 
             });
-            Log.d("Hi Mom", "Yo Yo Yo");
         } catch (Exception e) {
-            Log.d("Hi Mom", "Error during Zoho SalesIQ initialization: " + e.getMessage());
+            Log.d("LOGS", "CATCH: " + e.getMessage());
         }
     }
 
-    public String getHelloWorld() {
-        return message;
-    }
 }
